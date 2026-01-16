8eb3ed969775b529e4f4118e7e21b31524721a5c
Louis-Charles Biron <louilresbiron6@gmail.com>
2026-01-16 11:37:44 -0500

    Updated getAge function for more precision

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
-function getAge(birthYear) {
-    return new Date().getFullYear()-birthYear
+function getAge(birthYear, month) {
+    return ((new Date().getTime()-new Date(birthYear+"-"+month+"-01").getTime())/(1000*60*60*24*365.25))|0
 }
